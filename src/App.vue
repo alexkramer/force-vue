1b36d9de5354b7a7a62c15509b526b124d7b2764
Kurt Graff <graff1994@gmail.com>
2016-05-09 23:30:41 -0400

    Added dropdown selection for all api choices, added support for random planet

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
     <div class="mdl-grid portfolio-max-width">
-      <view-person></view-person>
+
+    <select class="mdl-tabs__tab " v-model="selected">
+      <option v-for="item in apiSelect">
+        {{ item }}
+      </option>
+    </select>
+      <view-person v-if="selected == 'Person'"></view-person>
+      <view-planet v-if="selected == 'Planet'"></view-planet>
     </div>
   </div>
 </template>
 
 <script>
 import ViewPerson from './components/ViewPerson';
-
+import ViewPlanet from './components/ViewPlanet';
 export default {
   components: {
     ViewPerson,
+    ViewPlanet,
+  },
+  data() {
+    return {
+
+      apiSelect: ['Person', 'Planet', 'Starships', 'Vehicle'],
+      selected: 'Person',
+
+    };
   },
 };
+
 </script>
---
+++ src/components/ViewPlanet.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
+    <mdl-button class="randplanetBtn" v-mdl-ripple-effect colored icon raised v-on:click="fetchRandomPlanet">
+      <i class="material-icons">public</i> Random planet
+    </mdl-button>
+    <mdl-textfield floating-label="Name" :value.sync="planetData.name"></mdl-textfield>
+    <mdl-textfield floating-label="Rotation Period" :value.sync="planetData.rotation_period"></mdl-textfield>
+    <mdl-textfield floating-label="Orbital Period" :value.sync="planetData.orbital_period"></mdl-textfield>
+    <mdl-textfield floating-label="Diameter" :value.sync="planetData.diameter"></mdl-textfield>
+    <mdl-textfield floating-label="Climate" :value.sync="planetData.climate"></mdl-textfield>
+    <mdl-textfield floating-label="Gravity" :value.sync="planetData.gravity"></mdl-textfield>
+    <mdl-textfield floating-label="terrain" :value.sync="planetData.terrain"></mdl-textfield>
+    <mdl-textfield floating-label="Surface Water" :value.sync="planetData.surface_water"></mdl-textfield>
+    <mdl-textfield floating-label="Population" :value.sync="planetData.population"></mdl-textfield>
+  </div>
+</template>
+
+<script>
+import planet from '../api/planet';
+import { MdlButton, MdlTextfield, directives } from 'vue-mdl';
+
+export default {
+  components: {
+    MdlButton,
+    MdlTextfield,
+  },
+  directives,
+  data() {
+    return {
+      planetData: {
+        name: '',
+        rotation_period: '',
+        orbital_period: '',
+        diameter: '',
+        climate: '',
+        gravity: '',
+        terrain: '',
+        surface_water: '',
+        population: '',
+      },
+    };
+  },
+  methods: {
+    fetchRandomPlanet() {
+      const randomPlanetId = Math.floor((Math.random() * 61) + 1);
+      planet.fetch(randomPlanetId).then((planetData) => {
+        this.planetData = Object.assign(this.planetData, planetData);
+      });
+    },
+  },
+};
+</script>
