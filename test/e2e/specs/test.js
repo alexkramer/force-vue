867ffe20b8a64be2da42b05d5fecce30c1fd9865
jwu <jason.wu@dynatrace.com>
2016-05-17 11:05:23 -0400

    add missing files and test cases

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -7,6 +7,29 @@ module.exports = {
     .url('http://localhost:8080')
       .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.randPersonBtn')
+      .pause(5000)
       .end()
+  },
+
+  'startship e2e tests': function (browser) {
+    browser
+        .url('http://localhost:8080')
+        .waitForElementVisible('#app', 5000)
+        //.click('.mdl-tabs__tab')
+        .setValue('.mdl-tabs__tab','Starship')
+        .click('.mdl-tabs__tab')
+        //.click('option[value="Starship"]') //selects the option but doesn't click
+        .pause(1000)
+        .keys(['\uE006']) //hits the enter key.
+        .pause(3000)
+        .assert.elementPresent('.randStarshipBtn')
+        .click('.randStarshipBtn')
+        .waitForElementPresent('.mdl-textfield__input', 2000)
+        .pause(3000)
+        .getText(".mdl-textfield__input", function(result) {this.assert.notEqual(result,'')})
+            //this.assert.true(result.length > 0)})
+        .end()
   }
+
+
 }
